e10aa6af6ec97e8a5365bacb78e6ca083ce74538
Eduardo <renovatio36@hotmail.com>
2018-05-21 01:26:20 -0500

    01
    Agregado funciones de botones

--- app/src/main/java/com/mejorprogramacion/controles/Principal.java
+++ app/src/main/java/com/mejorprogramacion/controles/Principal.java
@@ -1,13 +1,59 @@
 package com.mejorprogramacion.controles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Principal extends AppCompatActivity {
+
+    Button BtnTextView, BtnEditText, BtnButton, BtnListView, BtnCalendar, BtnOptions, BtnImages, BtnTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        BtnTextView = (Button) findViewById(R.id.BtnTextView);
+        BtnTextView.setOnClickListener(new ClickEvent());
+
+        BtnEditText = (Button) findViewById(R.id.BtnEditText);
+        BtnEditText.setOnClickListener(new ClickEvent());
+
+        BtnButton = (Button) findViewById(R.id.BtnButton);
+        BtnButton.setOnClickListener(new ClickEvent());
+
+        BtnListView = (Button) findViewById(R.id.BtnListView);
+        BtnListView.setOnClickListener(new ClickEvent());
+
+        BtnCalendar = (Button) findViewById(R.id.BtnCalendar);
+        BtnCalendar.setOnClickListener(new ClickEvent());
+
+        BtnOptions = (Button) findViewById(R.id.BtnOptions);
+        BtnOptions.setOnClickListener(new ClickEvent());
+
+        BtnImages = (Button) findViewById(R.id.BtnImages);
+        BtnImages.setOnClickListener(new ClickEvent());
+
+        BtnTabs = (Button) findViewById(R.id.BtnTabs);
+        BtnTabs.setOnClickListener(new ClickEvent());
+
     }
+
+
+    class ClickEvent implements View.OnClickListener{
+        public void onClick(View Btn){
+            if(Btn == BtnTextView){
+                Intent i = new Intent(Principal.this,TextViewActivity.class);
+                startActivity(i);
+            }
+
+        }
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/mejorprogramacion/controles/TextViewActivity.java
+++ app/src/main/java/com/mejorprogramacion/controles/TextViewActivity.java
@@ -2,10 +2,11 @@ package com.mejorprogramacion.controles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class TextViewActivity extends AppCompatActivity {
+public class TextViewActivity extends AppCompatActivity{
 
     TextView TxtCambioTexto;
     Button BtnCambiarTexto;
@@ -16,9 +17,17 @@ public class TextViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_text_view);
 
         TxtCambioTexto = (TextView) findViewById(R.id.TxtCambioTexto);
-        BtnCambiarTexto = (Button) findViewById(R.id.BtnButton);
+        BtnCambiarTexto = (Button) findViewById(R.id.BtnCambiarTexto);
+        BtnCambiarTexto.setOnClickListener(new ClickEvent());
 
 
+    }
 
+    class ClickEvent implements View.OnClickListener{
+        public void onClick(View Btn){
+            if(Btn == BtnCambiarTexto){
+                TxtCambioTexto.setText("Este texto fue cambiado por código");
+            }
+        }
     }
 }
